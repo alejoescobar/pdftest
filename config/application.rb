b2509fce38b9f8f8603cbb15d6c306feac1c7961
Alejandro <alejandro@Alejandros-MacBook-Pro.local>
2014-03-03 00:42:50 -0500

    set precompile to false

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Pdftest
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.middleware.use "PDFKit::Middleware"
+    config.assets.initialize_on_precompile = false
   end
 end
 
