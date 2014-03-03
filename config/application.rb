65c800581008e1b6cbafebf51512877d0195eb1a
Alejandro <alejandro@Alejandros-MacBook-Pro.local>
2014-03-03 00:32:20 -0500

    Remove config.threadsage

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Pdftest
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.middleware.use "PDFKit::Middleware"
-    config.threadsafe!
   end
 end
 
