988d6ef89d8522b0fa2cc1f0a7b7e6cc671218e6
Alejandro Escobar <alejoescobac@gmail.com>
2014-03-18 23:37:39 -0500

    Test 2

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -2,9 +2,10 @@ PDFKit.configure do |config|
 PDFKit.configure do |config|
   config.default_options = { page_size: 'A4', print_media_type: true }
   if RUBY_PLATFORM =~ /linux/
-    wkhtmltopdf_executable = 'wkhtmltopdf-amd64'
+    wkhtmltopdf_executable = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s
   elsif RUBY_PLATFORM =~ /darwin/
-    wkhtmltopdf_executable = '/Users/alejandro/Projects/pdftest/pdftest/bin/wkhtmltopdf'
+    # wkhtmltopdf_executable = '/Users/alejandro/Projects/pdftest/pdftest/bin/wkhtmltopdf'
+    wkhtmltopdf_executable = Rails.root.join('vendor', 'wkhtmltopdf').to_s
   else
     raise "Unsupported. Must be running linux or intel-based Mac OS."
   end
